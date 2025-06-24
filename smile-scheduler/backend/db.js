9067db4190ed8c3f105b3cbe0c879ec26afc0902
Justin Alaan-Nguyen <justinalaan2@gmail.com>
2025-06-24 01:32:26 -0400

    backend setup p2

--- smile-scheduler/backend/db.js
+++ smile-scheduler/backend/db.js
@@ -1,12 +1,13 @@
-//backend/db.js
-const mysql = require('mysql2/promise'); // promise wrapper
+// backend/db.js
+const mysql = require('mysql2/promise');
+require('dotenv').config(); // Make sure this is called
 
 const pool = mysql.createPool({
-  host: 'smile-scheduler-db.clu2esaqsxaz.ca-central-1.rds.amazonaws.com',
-  user: 'admin',
-  password: 'Letplay123456789!',
-  database: 'smile_scheduler_db',
-  port: 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
